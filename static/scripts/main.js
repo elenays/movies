97710f6dd63201a5a318e96894cac27f94305949
elenays <helenasp@yandex.ru>
2018-11-14 17:20:06 +0300

    Реализован поиск фильма

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,9 +1,20 @@
-function showMovies () {
+function getMovies() {
     $('.movie').empty();
-    $('.movie').append( $(".enter-movie").val());
+    movie = $(".enter-movie").val();
     $('.enter-movie').val("");
+    DATAGLOBAL = movie;
+
+    $.ajax({
+         url: 'http://www.omdbapi.com/?t='+DATAGLOBAL+'&apikey=1622e12b',
+         type: 'GET',
+         dataType: 'json',
+     })
+
+    .done(function(data) {
+        console.log(data);
+        var templateScript = $('#movie').html();
+        var template = Handlebars.compile(templateScript);
+        $('.itemEntry').append(template(data));
+    });
+
 }
-
-
-
-
