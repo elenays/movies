ba1890b1befc67b0689f78ca394558d4c4c8e564
elenays <helenasp@yandex.ru>
2018-11-14 22:58:57 +0300

    Рефакторинг и обработка пустого ввода

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,20 +1,19 @@
-function getMovies() {
-    $('.movie').empty();
-    movie = $(".enter-movie").val();
+$('.find-movie').on('click', function () {
+    let movie = $(".enter-movie").val();
     $('.enter-movie').val("");
-    DATAGLOBAL = movie;
-
-    $.ajax({
-         url: 'http://www.omdbapi.com/?t='+DATAGLOBAL+'&apikey=1622e12b',
-         type: 'GET',
-         dataType: 'json',
-     })
-
-    .done(function(data) {
-        console.log(data);
-        var templateScript = $('#movie').html();
-        var template = Handlebars.compile(templateScript);
-        $('.itemEntry').append(template(data));
-    });
-
-}
+    if (!movie)
+        $('.movie__show').empty().append('<p class="movie__none">Enter movie!</p>');
+    else {
+        $.ajax({
+            url: 'http://www.omdbapi.com/?t='+ movie +'&apikey=1622e12b',
+            type: 'GET',
+            dataType: 'json',
+        })
+            .done(function(data) {
+                let templateScript = $('#movie').html();
+                let template = Handlebars.compile(templateScript);
+                $('.movie__show').empty().append(template(data));
+            });
+    }
+    
+})
